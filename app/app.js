f1b13f77dcd9180a9f420a8cd7740569baa34c96
Dawchih Liou <dawochih.liou@gmail.com>
2017-04-07 12:12:42 +0000

    front end multipart file upload

---
+++ app/app.js
@@ -0,0 +1,37 @@
+/*global $*/
+$(function() {
+    'use strict';
+    
+    var $form = $('form');
+    var $output = $('.output');
+    var $spinner = $('.spin');
+    
+    $form.on('submit', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        
+        var formData = new FormData($(this)[0]);
+        
+        $output.addClass('hidden');
+        $spinner.removeClass('hidden');
+        
+        $.ajax({
+            url: '/api/upload/',
+            method: 'post',
+            data: formData,
+            contentType: 'multipart/form-data',
+            processData: false
+        })
+        .done(function(data) {
+            $output.text('File Size: ' + data.filesize);
+        })
+        .fail(function(err) {
+           console.error(err);
+           $output.text(err.message)
+        })
+        .always(function() {
+            $output.removeClass('hidden');
+            $spinner.addClass('hidden');
+        });
+    });
+});
